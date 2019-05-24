771f635a9580d36a0c1533840311e3653a4c29c4
Hans-Gunther Schmidt <hans@otype.de>
2019-05-24 09:02:22 +0200

    Set base window/frame size

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -17,6 +17,12 @@
 ;; Turn on hl-line
 (global-hl-line-mode 1)
 
+;; Emacs 24.4 ships with a new minor mode called prettify-symbols-mode. Its purpose
+;; is to replace the standard text representation of various identifiers/symbols with
+;; a (arguably) more aesthetically pleasing representation (often a single unicode
+;; character would replace several ascii characters).
+(global-prettify-symbols-mode +1)
+
 ;;Set any color as the background face of the current line
 (set-face-background 'hl-line "#3e4446")
 
@@ -25,5 +31,18 @@
 
 ;; Set cursor color to bright yellow
 (set-cursor-color "#FEFF00")
+
+;; Set window frame size
+(if (display-graphic-p)
+    (progn
+      (setq initial-frame-alist
+            '(
+              (width . 150) ; chars
+              (height . 70))) ; lines
+      (setq default-frame-alist
+            '(
+              (width . 150)
+              (height . 70)))))
+
 
 (provide 'base-theme)
