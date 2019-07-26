91c4c5bb3484d10a94885b3a77416cc9a453e69b
Hans-Gunther Schmidt <hans@otype.de>
2019-07-26 14:02:31 +0200

    Configure elixir-mode (auto-format on save, etc.)

--- elisp/lang-elixir.el
+++ elisp/lang-elixir.el
@@ -11,6 +11,16 @@
 
 (use-package elixir-mode
   :ensure t
-  :defer t)
+  :defer t
+  :config
+  ;; Create a buffer-local hook to run elixir-format on save, only when we enable elixir-mode.
+  (add-hook 'elixir-mode-hook
+            (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
+  (add-hook 'elixir-format-hook (lambda ()
+                                  (if (projectile-project-p)
+                                     (setq elixir-format-arguments
+                                            (list "--dot-formatter"
+                                                  (concat (locate-dominating-file buffer-file-name ".formatter.exs") ".formatter.exs")))
+                                   (setq elixir-format-arguments nil)))))
 
 (provide 'lang-elixir)
