e9f638c88817cdfc0134d5e2f25ce8af2f8f3dc7
Hans-Gunther Schmidt <hans@otype.de>
2019-07-26 13:54:07 +0200

    Ensure elixir dependencies are installed automatically on fresh emacs setup

--- elisp/lang-elixir.el
+++ elisp/lang-elixir.el
@@ -1,13 +1,16 @@
 (use-package alchemist
+  :ensure t
   :defer t
   :config
   (add-hook 'elixir-mode-hook 'alchemist-mode))
 
 (use-package flycheck-mix
+  :ensure t
   :defer t
   :commands (flycheck-mix-setup))
 
 (use-package elixir-mode
+  :ensure t
   :defer t)
 
 (provide 'lang-elixir)
