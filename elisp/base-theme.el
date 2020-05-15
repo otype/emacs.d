90a40e261e127cd24a028e7f1b3fd9f40dafbfa9
Hans-Gunther Schmidt <hans@otype.de>
2020-05-15 21:54:38 +0200

    Add haskell formatting on save

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -11,7 +11,7 @@
   (moe-dark))
 
 ;; Font settings
-(set-frame-font "IBM Plex Mono 13" t t)
+(set-frame-font "IBM Plex Mono 11" t t)
 
 ;; HIGHLIGHTING
 ;; Turn on hl-line
--- elisp/lang-haskell.el
+++ elisp/lang-haskell.el
@@ -1,5 +1,9 @@
 (use-package haskell-mode
   :ensure t
-  :defer t)
+  :defer t
+  :config
+  (add-hook 'haskell-mode-hook #'hindent-mode)
+  (custom-set-variables
+   '(haskell-stylish-on-save t)))
 
 (provide 'lang-haskell)
