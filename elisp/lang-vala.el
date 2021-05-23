00c20ea1b9fccec46c9d3e29dd86bd456555d4e1
Hans-Gunther Schmidt <hans@otype.de>
2021-05-23 23:27:07 +0200

    Add Vala as new lang to emacs

---
+++ elisp/lang-vala.el
@@ -0,0 +1,9 @@
+(use-package vala-mode
+  :defer t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
+  (add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
+  (add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
+  (add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8)))
+
+(provide 'lang-vala)
