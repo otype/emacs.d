b24bc1ca8e29a7c5d6f623aaeeb803b4079826b8
Hans-Gunther Schmidt <hans@otype.de>
2022-10-13 23:58:50 +0200

    Enable smartparens for rust development

--- elisp/lang-rust.el
+++ elisp/lang-rust.el
@@ -10,6 +10,7 @@
               ("C-c C-c Q" . lsp-workspace-shutdown)
               ("C-c C-c s" . lsp-rust-analyzer-status))
   :config
+  (require 'smartparens-config)
   ;; uncomment for less flashiness
   ;; (setq lsp-eldoc-hook nil)
   ;; (setq lsp-enable-symbol-highlighting nil)
