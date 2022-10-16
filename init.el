598d23bc9fb84b2f9f0c0d0f5420660f442f114d
Hans-Gunther Schmidt <hans@otype.de>
2022-10-16 12:02:39 +0200

    Remove any traces of email (#5)

--- init.el
+++ init.el
@@ -4,9 +4,6 @@
 
 ;;; Code:
 
-;; Taken from here: https://github.com/bbatsov/prelude/issues/1225#issuecomment-511266025
-;(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -22,7 +19,6 @@
 (require 'base-global-keys)
 
 ;; Email
-;;(require 'email)
 
 ;; Language layers
 ;(require 'lang-elixir)
@@ -36,8 +32,3 @@
 (require 'lang-rust)
 ;(require 'lang-terraform)
 ;(require 'lang-vala)
-
-;; To speed up, compile to bytecode (see https://stackoverflow.com/a/8902202)
-;; DISABLE ON FIRST START OF EMACS (or when clearing ~/.emacs.d/elpa)
-;; If installation of elpa modules fails: $ find ~/.emacs.d -name *.elc | xargs rm -f
-;;(byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
