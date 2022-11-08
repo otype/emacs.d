265f898c3576c79a1dbe96fb872d119f2a11e2af
Hans-Gunther Schmidt <hans@otype.de>
2022-11-08 20:03:36 +0100

    Mini fix

--- elisp/base.el
+++ elisp/base.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("org"   . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 ;; use-package to simplify the config file
