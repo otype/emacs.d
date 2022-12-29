21d658fdb0fb0f9468b2a46403326c17a45a0d30
Hans-Gunther Schmidt <hans@otype.de>
2022-12-29 01:29:45 +0100

    Unset nerve-racking suspend-frame kdb shortcuts C-z (and C-x C-z)

--- elisp/base-global-keys.el
+++ elisp/base-global-keys.el
@@ -6,7 +6,14 @@
 		(lambda () (interactive) (find-file "~/org/mw-tasks.org")))
 (global-set-key (kbd "C-c C-c") 'org-capture)
 
-; see base-functions.el for this function
+;; Duplicate current line (or region)
 (global-set-key (kbd "C-c d") 'duplicate-current-line-or-region)
+
+;; Reload the emacs configuration
+(global-set-key (kbd "C-c <f12>") 'my-reload-emacs-configuration)
+
+;; Prevent frame to be suspended (totally nerve-racking)
+(global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "C-x C-z"))
 
 (provide 'base-global-keys)
