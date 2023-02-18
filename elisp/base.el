5d4540a57c8aea2bfbd5489fd0e6a903e00a5fa7
Hans-Gunther Schmidt <otyso@otype.de>
2023-02-18 22:16:49 +0100

    Add (commented) melpa-stable package-archive

--- elisp/base.el
+++ elisp/base.el
@@ -1,5 +1,7 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; Choose either melpa-stable or bleedinge-edge melpa
+;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org"   . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
