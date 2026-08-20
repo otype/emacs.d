746babc6eafb8d1f9e08dbbbba8ae8dea7275d73
Hans-Gunther Schmidt <hans.schmidt@meltwater.com>
2026-08-20 13:23:12 +0200

    Switching theme

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -25,13 +25,60 @@
   (setq doom-modeline-height 35)
   (setq doom-modeline-bar-width 1))
 
-;; Kaolin is a set of eye pleasing themes for GNU Emacs with support for a large
-;; number of modes and external packages.
-;; https://github.com/ogdenwebb/emacs-kaolin-themes
-(use-package kaolin-themes
+;; Highly accessible themes, conforming with the highest standard for colour
+;; contrast between background and foreground values (WCAG AAA). They also are
+;; optimised for users with red-green colour deficiency.
+(use-package modus-themes
+  :ensure t
+  :demand t
+  :init
+  ;; Starting with version 5.0.0 of the `modus-themes', other packages
+  ;; can be built on top to provide their own "Modus" derivatives.
+  ;; For example, this is what I do with my `ef-themes' and
+  ;; `standard-themes' (starting with versions 2.0.0 and 3.0.0,
+  ;; respectively).
+  ;;
+  ;; The `modus-themes-include-derivatives-mode' makes all Modus
+  ;; commands that act on a theme consider all such derivatives, if
+  ;; their respective packages are available and have been loaded.
+  ;;
+  ;; Note that those packages can even completely take over from the
+  ;; Modus themes such that, for example, `modus-themes-rotate' only
+  ;; goes through the Ef themes (to this end, the Ef themes provide
+  ;; the `ef-themes-take-over-modus-themes-mode' and the Standard
+  ;; themes have the `standard-themes-take-over-modus-themes-mode'
+  ;; equivalent).
+  ;;
+  ;; If you only care about the Modus themes, then (i) you do not need
+  ;; to enable the `modus-themes-include-derivatives-mode' and (ii) do
+  ;; not install and activate those other theme packages.
+  (modus-themes-include-derivatives-mode 1)
+  :bind
+  (("<f5>" . modus-themes-rotate)
+   ("C-<f5>" . modus-themes-select)
+   ("M-<f5>" . modus-themes-load-random))
   :config
-  (load-theme 'kaolin-shiva t)
-  (kaolin-treemacs-theme))
+  ;; Your customizations here.  All customizations must evaluated
+  ;; BEFORE loading the theme.
+  (setq modus-themes-to-toggle '(modus-operandi modus-vivendi)
+        modus-themes-to-rotate modus-themes-items
+        modus-themes-mixed-fonts t
+        modus-themes-variable-pitch-ui t
+        modus-themes-italic-constructs t
+        modus-themes-bold-constructs t
+        modus-themes-completions '((t . (bold)))
+        modus-themes-prompts '(bold)
+        modus-themes-headings
+        '((agenda-structure . (variable-pitch light 2.2))
+          (agenda-date . (variable-pitch regular 1.3))
+          (t . (regular 1.15))))
+
+  (setq modus-themes-common-palette-overrides nil)
+
+  ;; Finally, load your theme of choice (or a random one with
+  ;; `modus-themes-load-random', `modus-themes-load-random-dark',
+  ;; `modus-themes-load-random-light').
+  (modus-themes-load-theme 'modus-operandi))
 
 ;; Font settings
 (set-frame-font "IBM Plex Mono 13" t)
